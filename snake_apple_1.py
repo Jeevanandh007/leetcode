052a6b540acee0cf7321c5ec8edbdfde025710f1
Jeevanandh007 <jeevavijay24@gmail.com>
2024-10-31 00:35:47 +0000

    done

--- snake_apple_1.py
+++ snake_apple_1.py
@@ -1,19 +1,47 @@
 def minimumtime(k,position):
-   
+
     position.sort()
-    position.abs()
+    n=len(position)
+
+    time=[]
+
+    # position_abs=[]
+    # for i in position:
+    #     p = abs(i)
+    #     position_abs.append(p)
+
+#calculate time for each consecutive set of k apples
+    def calc_total_time(apple_set):
+        current_pos=0
+        total_time=0
+
+
+        for i in apple_set:
+            total_time=total_time + abs(i-current_pos) #0+ (-20-0) >> 20 + (-10--20) >>30 +(-10-5)=45
+            print("TT",total_time)
+            current_pos=i
+            #print(total_time)
+
+
+        
+        return total_time
+    
     
 
-    min_time=float('inf')
-    min_neg_items = max(0, k - len(pos_positions))
-    max_neg_items = min(k, len(neg_positions))
+#get consecutive positions of k apples
+    for i in range(n-k+1):         #n is total number of positions =
+                                # 5-3+1 if n is 5 and k is 3 in python
+                                # addition and substrction has equal precedence in python
 
-    for m in range (min_neg_items, ma\)
+        apple_set=position[i:i+k]
+        print("apple set ", apple_set)
+        #calc_total_time(0,apples_set)
+        time.append(calc_total_time(apple_set))
+        print("time for each set of consecutive apple intake by snake = " ,time)
+    return min(time)      
 
-    
-    
-    return total_time
 
-k = 5
-position = [-20, 5, 10,-5, 20]
-minimumtime(k,position)
+
+k = 3
+position = [-20, -10, 5, 10, 20]
+print("minimum time = ",minimumtime(k,position))
